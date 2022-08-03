d4098750c8ddc693b3f3fa2fa34c3a89fdd4ab24
Pablo Armando <pabloar24.7@hmail.com>
2022-08-03 19:18:32 -0300

    testing transition on scroll

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React , {useState } from 'react';
+import "./Home.css";
 
 const Home = () => {
+const [test , setTest] = useState('test');
+const [background , setBackground] = useState('homeAnimation');
+
+    function change () {
+
+        if(window.scrollY > 50){
+           setTest("test1"); 
+           setBackground("homeAnimation_active");
+        }else{
+            setTest("test");
+            setBackground("homeAnimation");
+        }  
+    }
+    window.addEventListener('scroll' , change)
     return (
         <div>
-            
+            <section>
+                <div  className={background}>
+                    <div className="homeAnimation__text">
+                        <h1  className={test}>Front end developer</h1>
+                        <p>bulding better worlds</p>
+                      </div>  
+
+                </div>
+
+            </section>
         </div>
     );
 };
